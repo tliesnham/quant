1e7082e29a267fa88e593bf039849db07e69cd8e
Taylor Liesnham <tliesnham@icloud.com>
2025-11-01 13:20:59 +0000

    add pair trades

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from stock import Stock
-from trading_strategy import MovingAverageStrategy
-from backtest_engine import BacktestEngine
+from trading_strategy import MovingAverageStrategy, PairMovingAverageStrategy
+from backtest_engine import BacktestEngine, PairBacktestEngine
 
 if __name__ == "__main__":
-    spy = Stock("SPY", "2020-01-01", "2025-01-01")
-    d20_ma = MovingAverageStrategy(spy)
+    #spy = Stock("SPY", "2020-01-01", "2025-01-01")
+    #d20_ma = MovingAverageStrategy(spy)
 
-    backtest = BacktestEngine(d20_ma)
-    backtest.run_backtest()
+    #backtest = BacktestEngine(d20_ma)
+    #backtest.run_backtest()
+
+    aapl = Stock("AAPL", "2020-01-01", "2025-01-01")
+    gld = Stock("GLD", "2020-01-01", "2025-01-01")
+    aapl_ma = PairMovingAverageStrategy(aapl, gld)
+
+    pair_backtest = PairBacktestEngine(aapl_ma)
+    pair_backtest.run_backtest()
--- trading_strategy.py
+++ trading_strategy.py
@@ -24,3 +24,19 @@ class MovingAverageStrategy(TradingStrategy):
             elif day.SMA_20 < day.Close:
                 signal = 1
             yield (day.Index, signal)
+
+class PairMovingAverageStrategy(TradingStrategy):
+    def __init__(self, signal_stock: Stock, pair_stock: Stock):
+        super().__init__(signal_stock)
+        self.pair_stock = pair_stock
+
+    def generate_signals(self):
+        signal_data = self.stock.data
+
+        for day in signal_data.itertuples():
+            signal = 0
+            if day.SMA_20 > day.Close:
+                signal = -1
+            elif day.SMA_20 < day.Close:
+                signal = 1
+            yield (day.Index, signal)
