fdf65ad0f5c1ea5852ded279291dac25b93c2cf5
Taylor Liesnham <tliesnham@icloud.com>
2025-11-03 11:03:12 +0000

    refactor constants for start end dates

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from trading_strategy import MovingAverageStrategy
 from backtest_engine import BacktestEngine
 
 if __name__ == "__main__":
-    aapl = Stock("AAPL", "2020-01-01", "2025-01-01")
-    tqqq = Stock("TQQQ", "2020-01-01", "2025-01-01")
+    START_DATE = "2020-01-01"
+    END_DATE = "2025-01-01"
+
+    aapl = Stock("AAPL", START_DATE, END_DATE)
+    tqqq = Stock("TQQQ", START_DATE, END_DATE)
+
     aapl_ma = MovingAverageStrategy(aapl)
 
     pair_backtest = BacktestEngine([aapl, tqqq], aapl_ma)
